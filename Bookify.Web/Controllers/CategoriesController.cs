0074b641d885aee1774c3f824840e92d674059c0
Abdelrazzaq <abdotaha12345m@gmail.com>
2026-05-17 10:49:59 +0300

    Add CategoriesController Add Categories index.html

---
+++ Bookify.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Bookify.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Web.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            //TO DO: Make ViewModel 
+            var Categories = _context.Categories;
+            return View(Categories);
+        }
+    }
+}
